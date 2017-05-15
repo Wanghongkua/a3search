e2e5eb3567b2957bdff20f90071f5020195aa09b
Wanghongkuan <728261774@qq.com>
2017-05-15 15:32:32 +1000

    stable stem and count frequency of files

--- process_index.c
+++ process_index.c
@@ -21,6 +21,18 @@ int isAlpha(char *c_char)
     return 0;
 }
 
+void append_index(char *c_stem, char *argv[], char *index_file_name)
+{
+    FILE *index_file;
+
+    compute_file_name(argv[2], c_stem, index_file_name);
+
+    /*index_file = fopen(index_file_name, "a");*/
+
+    /*fclose(index_file);*/
+    printf("%s\n", index_file_name);
+}
+
 /*
  *create index files
  */
@@ -48,12 +60,15 @@ void create_index(char *flag_file_name, char *argv[])
 
     FILE *origin_file;
 
-    char *buffer;
     char *c_char;
     char *c_word;
     char *c_stem;
+
+    char *buffer;
     unsigned int read_bytes = 0;
     unsigned int i;
+
+    struct word_frequency *root;
 
     buffer = malloc(sizeof(char) * BUFFER_SIZE);
 
@@ -72,8 +87,14 @@ void create_index(char *flag_file_name, char *argv[])
         if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0) {
             continue;
         }
+
         compute_file_name(argv[1], dp->d_name, origin_file_name);
-        /*printf("%s\n", origin_file_name);*/
+
+        root = malloc(sizeof(struct word_frequency));
+        root->next = NULL;
+        root->c_stem = NULL;
+        root->frequency = 0;
+
         origin_file = fopen(origin_file_name, "r");
         while ((read_bytes = fread(buffer, 1, BUFFER_SIZE, origin_file))) {
             for (i = 0; i < read_bytes; ++i) {
@@ -83,15 +104,18 @@ void create_index(char *flag_file_name, char *argv[])
                 }else{
                     if (strlen(c_word) >= 3) {
                         get_stem(c_word, c_stem);
-                        printf("%s\t\t%s\n", c_word, c_stem);
-                        /* TODO: append_index */
-                        /*append_index(c_stem);*/
+
+                        /*printf("%s\t\t%s\n", c_word, c_stem);*/
+                        add_to_root(c_stem, root);
+                        /*append_index(c_stem, argv, index_file_name);*/
                         /*c_stem[0] = '\0';*/
                     }
                     c_word[0] = '\0';
                 }
             }
         }
+        /* TODO: save root to file */
+        free_word_frequency(root);
         fclose(origin_file);
     }
 
@@ -105,6 +129,51 @@ void create_index(char *flag_file_name, char *argv[])
     free(c_stem);
     free(origin_file_name);
     free(index_file_name);
+}
+
+void free_word_frequency(struct word_frequency *root)
+{
+    if (root->next != NULL) {
+        free_word_frequency(root->next);
+    }
+    /* TODO: delete those 3 lines */
+    /*if (root->c_stem != NULL) {*/
+        /*printf("%s\t%d\n", root->c_stem, root->frequency);*/
+    /*}*/
+    free(root);
+}
+
+void add_to_root(char *c_stem, struct word_frequency *root)
+{
+    struct word_frequency *next_word;
+    next_word = root;
+    /*while (next_word->c_stem != NULL) {*/
+    while (1) {
+        if (next_word->c_stem == NULL) {
+            next_word->c_stem = malloc(sizeof(char) * (strlen(c_stem + 1)));
+            strcpy(next_word->c_stem, c_stem);
+            next_word->frequency = 0;
+            next_word->next = NULL;
+            break;
+        }
+        if (strcmp(next_word->c_stem, c_stem) == 0) {
+            break;
+        }
+        if (next_word->next == NULL) {
+            next_word->next = malloc(sizeof(struct word_frequency));
+            next_word = next_word->next;
+
+            next_word->next = NULL;
+            next_word->frequency = 0;
+            next_word->c_stem = malloc(sizeof(char) * (strlen(c_stem + 1)));
+            strcpy(next_word->c_stem, c_stem);
+            /*printf("%s\n", next_word->c_stem);*/
+
+            break;
+        }
+        next_word = next_word->next;
+    }
+    next_word->frequency += 1;
 }
 
 /*
--- process_index.h
+++ process_index.h
@@ -4,3 +4,13 @@ extern int exist_index(char *flag_file_name);
 extern void create_index(char *flag_file_name, char *argv[]);
 extern char * compute_flag_file(char *dir);
 extern int isAlpha(char *c_char);
+extern void append_index(char *c_stem, char *argv[], char *index_file_name);
+
+struct word_frequency {
+    char *c_stem;
+    unsigned int frequency;
+    struct word_frequency *next;
+};
+
+extern void add_to_root(char *c_stem, struct word_frequency *root);
+extern void free_word_frequency(struct word_frequency *root);
